597b7cd5ee4c0b7115ad3e338399393ef057ad51
kihan2518B <145173622+kihan2518B@users.noreply.github.com>
2023-11-21 22:43:00 +0530

    Update db.cjs

--- servers/database/db.cjs
+++ servers/database/db.cjs
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 // const URI = "mongodb://localhost:27017/mern_admin";
-
+//this for mongo compass {local} change mern_admin to your database name
 const connectDb = async () => {
 
     const URI = `mongodb+srv://kp648027:Kishan@cluster0.spvkpc0.mongodb.net/?retryWrites=true&w=majority`;
-
+    //this for mongo cloud
+//change value of {kp648027} to Your username and {Kishan} to Your password and{cluster0} to Your clustername
 
     try {
         await mongoose.connect(URI);
